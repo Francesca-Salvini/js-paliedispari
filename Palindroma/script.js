4bec616ab9876c6d5a22d4326d7ef40bd5fb5dec
Francesca Salvini <franci98sp@gmail.com>
2021-03-25 15:13:36 +0100

    aggiunti più commenti nel file js

--- Palindroma/script.js
+++ Palindroma/script.js
@@ -5,7 +5,10 @@
 
 var parolaUtente = prompt("Dimmi una parola");
 
-    
+// step 2 : creare una funzione per capire se la parola inserita è palindroma
+
+// Parola da sinistra verso destra
+
 for( var i = 0; i < parolaUtente.length; i++ ) {
     // console.log(parolaUtente[i]);
     
@@ -13,6 +16,7 @@ for( var i = 0; i < parolaUtente.length; i++ ) {
     console.log(parolaDritta);
 }
 
+// Parola da destra verso sinistra
 
 for( var i = parolaUtente.length -1; i >= 0; i-- ) {
     // console.log(parolaUtente[i]);
@@ -20,6 +24,8 @@ for( var i = parolaUtente.length -1; i >= 0; i-- ) {
     var parolaAlContrario = parolaUtente[i];
     console.log(parolaAlContrario); 
 }
+
+// Con la funzione confronto parolaDritta e parolaAlContrario
 
 function valutaSePalindroma() {
     var risultato;
@@ -33,6 +39,8 @@ function valutaSePalindroma() {
     return risultato;
     
 }
+
+// Output con console.log del risultato
 
 var parolaValutata = valutaSePalindroma(parolaUtente);
 console.log( "La parola è " + parolaValutata);
